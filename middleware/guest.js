79c9e7771528167bbd4aaa1637875d31d5213537
nevevv <103154860+nevevv@users.noreply.github.com>
2023-01-23 15:57:01 +0500

    Update guest.js

--- middleware/guest.js
+++ middleware/guest.js
@@ -4,10 +4,8 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const store = useMainStore();
 
   if (store.usersToken) {
-    console.log(1);
     navigateTo(to.fullPath);
   } else {
-    console.log(2);
     navigateTo("/login");
   }
 });
