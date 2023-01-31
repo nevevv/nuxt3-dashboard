43317ea9492e990a28d84cccc0ce6242eeb87aa4
nevevv <103154860+nevevv@users.noreply.github.com>
2023-01-31 16:51:06 +0500

    fix

--- middleware/guest.js
+++ middleware/guest.js
@@ -4,10 +4,10 @@ import {useRuntimeConfig} from "#imports";
 
 export default defineNuxtRouteMiddleware((to, from) => {
     const cookie = useCookie('token');
-    const useStore = useMainStore()
-    const api_url = useRuntimeConfig().public.api_url
+    const useStore = useMainStore();
+    const api_url = useRuntimeConfig().public.api_url;
 
-    if(!cookie.value) navigateTo('/login')
+    if(!cookie.value) navigateTo('/login');
 
     if(to.fullPath === from.fullPath) {
         getUsersData(cookie,useStore,api_url)
@@ -17,7 +17,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
 });
 
 async function getUsersData(cookie,store,api_url) {
-    const response =  await $fetch(api_url+'/user', {
+    const response = await $fetch(api_url+'/user', {
         headers: {
             'Content-type': 'application/json',
             'Accept': 'application/json',
