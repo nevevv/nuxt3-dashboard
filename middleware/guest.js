09de72d90d5aa25c52a7e8126cef61f57e3b9d80
nevevv <103154860+nevevv@users.noreply.github.com>
2023-01-31 17:53:46 +0500

    Update guest.js

--- middleware/guest.js
+++ middleware/guest.js
@@ -1,34 +1,26 @@
-import {useMainStore} from "~/store";
-import {useRuntimeConfig} from "#imports";
-
+import { useMainStore } from "~/store";
+import { useRuntimeConfig } from "#imports";
 
 export default defineNuxtRouteMiddleware((to, from) => {
-    const cookie = useCookie('token');
-    const useStore = useMainStore();
-    const api_url = useRuntimeConfig().public.api_url;
+  const cookie = useCookie("token");
+  const useStore = useMainStore();
+  const api_url = useRuntimeConfig().public.api_url;
 
-    console.log(123124812309482390582323948230948230948023984029384200)
+  navigateTo("/login");
 
-    if(!cookie.value) {
-        navigateTo('/login')
-    };
-
-    if(to.fullPath === from.fullPath) {
-        getUsersData(cookie,useStore,api_url)
-    }
-
-
+  if (to.fullPath === from.fullPath) {
+    getUsersData(cookie, useStore, api_url);
+  }
 });
 
-async function getUsersData(cookie,store,api_url) {
-    const response = await $fetch(api_url+'/user', {
-        headers: {
-            'Content-type': 'application/json',
-            'Accept': 'application/json',
-            "Authorization": "Bearer "+cookie.value
-        },
-    });
+async function getUsersData(cookie, store, api_url) {
+  const response = await $fetch(api_url + "/user", {
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer " + cookie.value,
+    },
+  });
 
-    store.usersData = response
+  store.usersData = response;
 }
-
